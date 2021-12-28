543944e211d07ff18a6eb050e2f9c5c7d0625ecc
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2021-12-28 10:39:07 +0530

    Time: 8 ms (96.71%), Space: 14.6 MB (76.66%) - LeetHub

---
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int get_min_taps(int n, vector<int>& ranges){
+        int l = ranges.size(), tap_count = 0;
+        for(int i = 0, end = 0, max_reach = 0; end < n; end = max_reach){
+            tap_count++;
+            while(i < l && i <= end){
+                max_reach = max(max_reach, ranges[i]);
+                i++;
+            }
+            if(end == max_reach)    return -1;
+        }
+        return tap_count;
+    }
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int> left_shift_ranges(n + 1, 0);
+        int l = ranges.size();
+        for(int i = 0; i < l; i++){
+            if(ranges[i] > 0){
+                int left = max(0, i - ranges[i]);
+                left_shift_ranges[left] = max(left_shift_ranges[left], i + ranges[i]);
+            }
+        }
+        
+        return get_min_taps(n, left_shift_ranges);
+    }
+};
