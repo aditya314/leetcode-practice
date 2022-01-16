2205411caaa027f299750ae46409ef1248a012b2
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-01-16 15:22:25 +0530

    Time: 66 ms (5.38%), Space: 58.6 MB (5.08%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -5,10 +5,9 @@ class Solution {
         for(String s : votes){
             for(int pos = 0; pos < n; pos++){
                 char ch = s.charAt(pos);
+                voteMap.putIfAbsent(ch, new ArrayList<Integer>(Collections.nCopies(n, 0)));
                 ArrayList<Integer> positionVoteCount = voteMap.get(ch);
-                if(positionVoteCount == null)   positionVoteCount = new ArrayList<Integer>(Collections.nCopies(n, 0));
                 positionVoteCount.set(pos, positionVoteCount.get(pos) + 1);
-                voteMap.put(ch, positionVoteCount);
             }
         }
         
