c9c76726b895929deed1e93868533c4ffe6e94ff
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-01-21 13:37:02 +0530

    Time: 80 ms (86.14%), Space: 18.1 MB (76.11%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {  
 private:
-    static const long MOD = 10e9 + 7;
+    static const long MOD = 1e9 + 7;
     static const long random_prime = 101;
     long modular_exponentiation(long base, long power){
         long res = 1;
@@ -14,20 +14,20 @@ private:
 public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         unordered_map<char, int> window_count;
-        unordered_map<string, int> string_occurence;
+        unordered_map<long, int> string_occurence;
         int start = 0, max_freq = 0;
-        // long hash = 0, hash_rollback = modular_exponentiation(random_prime, minSize - 1) % MOD;
+        long hash = 0, hash_rollback = modular_exponentiation(random_prime, minSize - 1);
         for(int end = 0; end < s.length(); end++){
             window_count[s[end]]++;
-            // hash = (hash % MOD * random_prime + (s[end] - 'a')) % MOD;
             if((end - start + 1) > minSize){
-                //hash = (hash % MOD -  ((s[start] - 'a') * hash_rollback) % MOD) % MOD;
+                hash = (hash -  ((s[start] - 'a') * hash_rollback) % MOD) % MOD;
                 window_count[s[start]]--;
                 if(window_count[s[start]] == 0)   window_count.erase(s[start]);
                 start++;
             }
+            hash = ((hash * random_prime) % MOD + (s[end] - 'a')) % MOD;
             if((end - start + 1) == minSize && window_count.size() <= maxLetters)
-                max_freq = max(max_freq, ++string_occurence[s.substr(start,end - start + 1)]);
+                max_freq = max(max_freq, ++string_occurence[hash]);
         }
         return max_freq;
     }
