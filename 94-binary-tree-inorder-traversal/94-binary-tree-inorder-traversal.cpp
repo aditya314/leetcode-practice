ff4a0e80b63684a67ad7e099122e002ba7829e13
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-01-26 15:56:23 +0530

    Time: 8 ms (5.14%), Space: 8.2 MB (97.57%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -10,19 +10,49 @@
  * };
  */
 class Solution {
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     stack<TreeNode*> st;
+    //     TreeNode* curr = root;
+    //     vector<int> traversal;
+    //     while(curr || !st.empty()){
+    //         while(curr){
+    //             st.push(curr);
+    //             curr = curr -> left;
+    //         }
+    //         curr = st.top();    st.pop();
+    //         traversal.push_back(curr->val);
+    //         curr = curr -> right;
+    //     }
+    //     return traversal;
+    // }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
         TreeNode* curr = root;
         vector<int> traversal;
-        while(curr || !st.empty()){
-            while(curr){
-                st.push(curr);
-                curr = curr -> left;
+        while(curr){
+            if(curr -> left == NULL){
+                traversal.push_back(curr->val);
+                curr = curr -> right;
             }
-            curr = st.top();    st.pop();
-            traversal.push_back(curr->val);
-            curr = curr -> right;
+            else{
+                TreeNode* inorder_successor = curr->left;
+                while(inorder_successor->right && inorder_successor->right != curr)
+                        inorder_successor = inorder_successor -> right;
+                if(inorder_successor -> right == NULL){
+                    //create thread
+                    inorder_successor -> right = curr;
+                    curr = curr -> left;
+                }
+                else if(inorder_successor->right == curr){
+                    //remove thread
+                    inorder_successor -> right = NULL;
+                    traversal.push_back(curr->val);
+                    curr = curr -> right;
+                }
+                else{
+                    cout<<"Threading went wrong!!";
+                }
+            }
         }
         return traversal;
     }
