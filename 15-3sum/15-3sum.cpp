938e0e97faf2b0bb9c791a40237ceaf91271b89a
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-01-26 16:49:57 +0530

    Time: 85 ms (71.71%), Space: 19.9 MB (96.54%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -3,7 +3,7 @@ private:
     void get_two_sum(vector<int>& nums, int i, int target_sum, vector<vector<int>>& three_sum_list){
         int low = i + 1, high = nums.size() - 1;
         while(low < high){
-            int sum = nums[i] + nums[low] + nums[high];
+            int sum = nums[low] + nums[high];
             if(sum < target_sum)        low++;
             else if(sum > target_sum)   high--;
             else{
@@ -22,7 +22,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             //since all number after this will also be >. target_sum, thus never giving a valid answer 
             if(nums[i] > target_sum)    break;
-            if(i == 0 || nums[i - 1] != nums[i])     get_two_sum(nums, i, target_sum, three_sum_list);
+            if(i == 0 || nums[i - 1] != nums[i])     get_two_sum(nums, i, target_sum - nums[i], three_sum_list);
         }
         return three_sum_list;
     }
