88d48b8e48faf12b66a309c9feca403ec8c72665
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-02-03 13:25:09 +0530

    Time: 11 ms (78.25%), Space: 8.4 MB (57.18%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -6,9 +6,9 @@ public:
         int start = 0, end = 0;
         while(end < n){
             if(char_index.find(s[end]) != char_index.end())
-                start = max(start, char_index[s[end]]);
+                start = max(start, char_index[s[end]] + 1);
             max_len = max(max_len, end - start + 1);
-            char_index[s[end]] = end + 1;
+            char_index[s[end]] = end;
             end++;
         }
         return max_len;
