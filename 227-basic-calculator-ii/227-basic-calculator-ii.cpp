459167a834064dbbf693dd96537a757280726d2a
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-02-10 22:25:20 +0530

    Time: 27 ms (13.69%), Space: 8.1 MB (69.00%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+private:
+    unordered_map<char, int> precedence;
+    void set_precedence_rules(){
+        precedence['('] = -1;
+        precedence['+'] = 1;
+        precedence['-'] = 1;
+        precedence['*'] = 2;
+        precedence['/'] = 2;
+        precedence['^'] = 3;
+    }
+    int compute_stacks(stack<char>& operators, stack<int>& operands){
+        int a = operands.top();
+        operands.pop();
+        int b = operands.top();
+        operands.pop();
+        char ch = operators.top();
+        operators.pop();
+        if(ch == '/' && a != 0)       return b / a;
+        else if(ch == '+')  return a + b;
+        else if(ch == '-')  return b - a;
+        else if(ch == '*')  return a * b;
+        else if(ch == '^')  return (int)pow(b, a);
+        return 0;
+    }
+public:
+    int calculate(string s) {
+        stack<char> operators;
+        stack<int> operands;
+        int i, n = s.size();
+        set_precedence_rules();
+        for(i = 0 ; i < n; i++){
+            if(isdigit(s[i])){
+                int val = s[i] - '0';
+                //move only if you see a digit
+                while(i + 1 < n && isdigit(s[i + 1])){
+                    val = val * 10 + (s[i + 1] -'0');
+                    i++;
+                }
+                operands.push(val);
+            }
+            else if(s[i] == ' ')    continue;
+            else if(s[i] == '(')    operators.push(s[i]);
+            else if(s[i] == ')'){
+                while(operators.top() != '(')
+                    operands.push(compute_stacks(operators, operands));
+                //removing '('
+                operators.pop();
+            }
+            else{
+                //s[i] is a operator
+                while(!operators.empty() && precedence[s[i]] <= precedence[operators.top()])
+                            operands.push(compute_stacks(operators, operands));
+                operators.push(s[i]);
+            }
+        }
+        while(!operators.empty())
+                operands.push(compute_stacks(operators, operands));
+        return operands.top();
+    }
+};
