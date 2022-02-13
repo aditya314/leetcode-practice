b6281230e3dabc5031f639ad2650b6f16b2318c5
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-02-13 19:59:33 +0530

    Time: 401 ms (40.17%), Space: 30.4 MB (15.30%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,6 +1,5 @@
 class Solution {
 private:
-    int answer = INT_MAX;
     unordered_map<string, vector<int>> transformation_graph;
     void build_transformation_graph(vector<string>& wordList){
         for(int i = 0; i < wordList.size(); i++){
@@ -20,27 +19,28 @@ private:
             cout<<endl;
         }
     }
-    int hop_from_node(queue<string>& q, unordered_map<string, int>& visited_level, unordered_map<string, int>& other_visited_level, vector<string>& word_list){
-        string parent_string = q.front();
-        cout<<"For word :"<<parent_string<<endl;
-        q.pop();
-        int level = visited_level[parent_string];
-        for(int j = 0; j < parent_string.length(); j++){
-            char ch = parent_string[j];
-            parent_string[j] = '*';
-            for(int child_index : transformation_graph[parent_string]){
-                string child_word = word_list[child_index];
-                if(other_visited_level.find(child_word) != other_visited_level.end()){
-                    this->answer = min(this->answer, level + other_visited_level[child_word]);
-                    // return level + other_visited_level[child_word];
-                }if(visited_level.find(child_word) == visited_level.end()){
-                    visited_level[child_word] = level + 1;
-                    q.push(child_word);
+    int bfs_one_layer(queue<string>& q, unordered_map<string, int>& visited_level, unordered_map<string, int>& other_visited_level, vector<string>& word_list){
+        for(int i = 0; i < q.size(); i++){
+            string parent_string = q.front();
+            q.pop();
+            int level = visited_level[parent_string];
+            for(int j = 0; j < parent_string.length(); j++){
+                char ch = parent_string[j];
+                parent_string[j] = '*';
+                for(int child_index : transformation_graph[parent_string]){
+                    string child_word = word_list[child_index];
+                    if(other_visited_level.find(child_word) != other_visited_level.end())
+                        return level + other_visited_level[child_word];
+                    if(visited_level.find(child_word) == visited_level.end()){
+                        visited_level[child_word] = level + 1;
+                        q.push(child_word);
+                    }
+
                 }
+                parent_string[j] = ch;
             }
-            parent_string[j] = ch;
         }
-        return 0;
+        return -1;
     }
     int get_length_bidirectional_bfs(string& begin_word, string& end_word, vector<string>& word_list){
         queue<string> q_begin, q_end;
@@ -53,12 +53,11 @@ private:
         
         int answer = 0;
         while(!q_begin.empty() && !q_end.empty()){
-            answer = hop_from_node(q_begin, visited_level_begin, visited_level_end, word_list);
-            cout<<answer<<endl;
-            if(answer > 0) return answer;
-            answer = hop_from_node(q_end, visited_level_end, visited_level_begin, word_list);
-            cout<<answer<<endl;
-            if(answer > 0) return answer;
+            if(q_begin.size() <= q_end.size())
+                answer = bfs_one_layer(q_begin, visited_level_begin, visited_level_end, word_list);
+            else
+                answer = bfs_one_layer(q_end, visited_level_end, visited_level_begin, word_list);
+            if(answer != -1) return answer;
         }
         return 0;
     }
@@ -66,12 +65,10 @@ public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         if(find(wordList.begin(), wordList.end(), endWord) == wordList.end())
             return 0;
-        this->answer = INT_MAX;
         //regex -> index in array for possible tranformation
         transformation_graph.clear();
         build_transformation_graph(wordList);
         print_graph();
-        get_length_bidirectional_bfs(beginWord, endWord, wordList);
-        return this->answer == INT_MAX ? 0: this->answer;
+        return get_length_bidirectional_bfs(beginWord, endWord, wordList);
     }
 };
