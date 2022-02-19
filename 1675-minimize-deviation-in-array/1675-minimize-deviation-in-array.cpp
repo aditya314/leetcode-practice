12d6627081d11ca4e80b8c5c25badf1c4f6331a8
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-02-19 11:59:10 +0530

    Time: 298 ms (56.29%), Space: 71.2 MB (83.44%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        int res = INT_MAX, min_number = INT_MAX;
+        int min_deviation = INT_MAX, min_number = INT_MAX;
+        //max heap
         priority_queue<int> pq;
         for (int n : nums) {
+            //only odd numbers can be maximised
             if(n % 2 == 1)     n = n << 1;
             pq.push(n);
             min_number = min(min_number, n); 
         }
-        while (pq.top() % 2 == 0) {
-            res = min(res, pq.top() - min_number);
-            min_number = min(min_number, pq.top() / 2);
-            pq.push(pq.top() / 2);
+        //there are only two ways to decrease deviation: decrease maximum, or increase minimum.
+        // If we have increased all numbers to their maximum, then we can not increase minimum. If we want smaller deviation, we can only decrease maximum.
+        while (!pq.empty()) {
+            int current_max = pq.top();
             pq.pop();
+            min_deviation = min(min_deviation, current_max - min_number);
+            if(current_max % 2 == 0){
+                current_max = current_max >> 1;
+                min_number = min(min_number, current_max);
+                pq.push(current_max);
+            }
+            else // odd numbers cannot be minimised
+                break;
         }
-        res = min(res, pq.top() - min_number);
-        return res;
+        return min_deviation;
     }
 };
