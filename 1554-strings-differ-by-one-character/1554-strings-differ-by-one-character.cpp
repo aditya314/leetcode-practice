3f16f2773b85b9769e0bfa9c74619aa6115703c1
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-03-24 14:50:45 +0530

    Time: 522 ms (59.03%), Space: 148.5 MB (48.57%) - LeetHub

---
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool differByOne(vector<string>& dict) {
+        unordered_set<string> regex_set;
+        
+        for(string word : dict) {
+            string regex = word;
+            for(int i = 0; i < word.size(); i++) {
+                //resetting previous '*'
+                if(i > 0) 
+                    regex[i - 1] = word[i - 1];
+                regex[i] = '*';
+                if(regex_set.find(regex) != regex_set.end())
+                    return true;
+                regex_set.insert(regex);
+            }
+        }
+        return false;
+    }
+};
