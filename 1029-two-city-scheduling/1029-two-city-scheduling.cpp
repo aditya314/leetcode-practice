e392a93da988138bc910b507466e12760b1c9bb6
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-03-25 16:27:45 +0530

    Update 1029-two-city-scheduling.cpp

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    static bool cost_comparator(vector<int>& cost1, vector<int>& cost2){
-        return (cost1[0] - cost1[1]) < (cost2[0] - cost2[1]);
+    static bool cost_savings_for_city_A_comparator(const vector<int>& cost1, const vector<int>& cost2){
+        int savings1 = cost1[0] - cost1[1], savings2 = cost2[0] - cost2[1];
+        return (savings1 < savings2);
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        sort(costs.begin(), costs.end(), cost_comparator);
-        int n = costs.size() / 2;
-        int total_cost = 0;
-        for(int i = 0; i < n; i++){
-            total_cost += costs[i][0] + costs[i + n][1];
-        }
+        sort(costs.begin(), costs.end(), cost_savings_for_city_A_comparator);
+        
+        int total_cost = 0, n = costs.size() / 2;
+        //first n got to city A, next n got to city B
+        for(int i = 0; i < n; i++)
+            total_cost += (costs[i][0] + costs[i + n][1]);
         return total_cost;
     }
 };
