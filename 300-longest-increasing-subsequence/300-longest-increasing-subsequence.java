8f7d6a34e116e84b8259899104d0cb43db34f513
Aditya Nihal Kumar Singh <anksshady@gmail.com>
2022-05-25 11:50:26 +0530

    Time: 9 ms (76.65%), Space: 44.7 MB (33.26%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> piles = new ArrayList<>(nums.length);
+        for (int num : nums) {
+            int pile = Collections.binarySearch(piles, num);
+            if (pile < 0) 
+                pile = ~pile;
+            if (pile == piles.size())
+                piles.add(num);
+            else
+                piles.set(pile, num);
+        }
+        return piles.size();
+    }
+}
